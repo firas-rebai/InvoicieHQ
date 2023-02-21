0059e9feb8518724c4ee5cf90c4d1cc04ff05cd9
firas <firasrebai01@gmail.com>
2023-02-21 12:40:09 +0100

    some changes

---
+++ src/app/_services/auth.guard.ts
@@ -0,0 +1,26 @@
+import {Injectable} from '@angular/core';
+import {ActivatedRouteSnapshot, CanActivate, Router, RouterStateSnapshot, UrlTree} from '@angular/router';
+import {Observable} from 'rxjs';
+import {DataSharingService} from "./data-sharing.service";
+
+@Injectable({
+	providedIn: 'root'
+})
+export class AuthGuard implements CanActivate {
+	constructor(private dataSharingService: DataSharingService, private router : Router) {
+	}
+
+	canActivate(
+		route: ActivatedRouteSnapshot,
+		state: RouterStateSnapshot): Observable<boolean | UrlTree> | Promise<boolean | UrlTree> | boolean | UrlTree {
+		// console.log(this.dataSharingService.isUserLoggedIn.value)
+		// if (this.dataSharingService.isUserLoggedIn.value || state.url == '/login') {
+		// 	return true;
+		// } else {
+		// 	this.router.navigate(['/login']);
+		// 	return false;
+		// }
+		return true;
+	}
+
+}
